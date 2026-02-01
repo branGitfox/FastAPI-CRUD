e9d9615ee2ae2874a363d0daa4217fa536b6c568
BranGitfox <vixfgit@gmail.com>
2026-02-02 01:20:20 +0300

    feat: creating a seeders for pushing pre-product

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from database import session, engine
 from database_models import Base
+import database_models
+from models import Product
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+# umm a mock data to push into the db cause don't want to create it manualy
+products = [
+    Product(id=1, name="Gun", description="M416 from pubg mobile", price=10.7)
+]
+def init_db():
+    db = session()
+    for product in products:
+        db.add(database_models.Product(**product.model_dump()))
+    db.commit()
+
+
 # fetching all products
 @app.get('/products')
 def get_products():
