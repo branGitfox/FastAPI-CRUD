3f3c63e0838665326e808161074f0a976f23cf4c
BranGitfox <vixfgit@gmail.com>
2026-02-02 01:40:27 +0300

    ref: changing create_session to sessionmaker

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import create_session
+from sqlalchemy.orm import create_session, sessionmaker
 
 db_url = 'mysql+pymysql://root@localhost:3306/shop_api'
 engine = create_engine(db_url)
-session = create_session(engine, autoflush=False, autocommit=False)
+session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 from database import session, engine
 from database_models import Base
 import database_models
@@ -10,7 +12,8 @@ Base.metadata.create_all(bind=engine)
 
 # umm a mock data to push into the db cause don't want to create it manualy
 products = [
-    Product(id=1, name="Gun", description="M416 from pubg mobile", price=10.7)
+    Product(id=1, name="Gun", description="M416 from pubg mobile", price=10.7),
+    Product(id=2, name="Knife", description="Knife from pubg mobile", price=9.7)
 ]
 def init_db():
     db = session()
@@ -19,6 +22,8 @@ def init_db():
         for product in products:
             db.add(database_models.Product(**product.model_dump()))
         db.commit()
+
+init_db()
 
 # a singleton db getter for dependecy injection
 def get_db():
@@ -30,5 +35,9 @@ def get_db():
 
 # fetching all products
 @app.get('/products')
-def get_products():
-    return 'hello world'
+def get_products(db: Session = Depends(get_db)):
+    if db.query(database_models.Product).count() == 0:
+        return {'message': 'No product found'}
+    else:
+        datas = db.query(database_models.Product).all()
+        return datas
